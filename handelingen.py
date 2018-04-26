abc341ff2fc16363b82d8b326a56b4761bd5a458
Nelson <hello@nelson.science>
2018-04-26 12:02:57 +0200

    Remove print statements and fill in vergaderingen_voor_jaar logic

--- handelingen.py
+++ handelingen.py
@@ -16,7 +16,7 @@ def insert_dash(original):
     return original[:4] + '-' + original[4:]
 
 def generate_ovz_url(vergaderjaar, volgnummer):
-    print('Generate overzicht url om aantal documenten uit te halen')
+    """Generate overzicht url om aantal documenten uit te halen"""
 
     vergaderjaar_dash = insert_dash(str(vergaderjaar))
     ovz_url = base_ovz_url + vergaderjaar_dash + '/' + str(volgnummer) + '/'
@@ -44,15 +44,12 @@ def download_document(url, soup=True):
 
 
 def fetch_aantal_vergaderitems(vergaderjaar, volgnummer):
-    print('Haal aantal publicaties voor vergadering van de website')
+    """Haal aantal publicaties voor vergadering van de website"""
 
     ovz_url = generate_ovz_url(vergaderjaar, volgnummer)
-    print(ovz_url)
     document = download_document(ovz_url, soup=False)
-    # print(document.text)
     match_aantal_items = re.search(regex_publicaties, document.text)
     aantal_items = int(match_aantal_items.group('aantal'))
-    print(aantal_items)
     return aantal_items
 
 
@@ -69,14 +66,49 @@ def fetch_alle_vergaderitems(vergaderjaar, volgnummer):
         print(document_title)
 
         # TODO: Do something with the retrieved document.
+        # Return it?
 
 
 def fetch_vergaderingen_voor_jaar(vergaderjaar):
-    # TODO: Voor alle vergaderingen in het jaar: haal alle items op.
     print('Fetch alle vergaderingen')
 
+    i = 60
+    while True:
+
+        print('Volgnummer: %i' % i)
+        aantal_items = fetch_aantal_vergaderitems(vergaderjaar, i)
+        print('Aantal items in deze vergadering: %i' % aantal_items)
+        aantal_items_next = fetch_aantal_vergaderitems(vergaderjaar, i + 1)
+        print('Aantal items in volgende vergadering: %i' % aantal_items_next)
+
+        # NOTE: Assume that if the result is not 0, then it is > 0.
+        # NOTE: Assume that if we have two zeros in a row, we reached the end of the year.
+        if (aantal_items != 0) and (aantal_items_next != 0):
+            print('We\'re still in the middle of it, woop!')
+        elif (aantal_items == 0) and (aantal_items_next != 0):
+            print('Looks like we\'re missing some data, woops...')
+        elif (aantal_items != 0) and (aantal_items_next == 0):
+            print('Huidige vergadering aanwezig, we gaan door.')
+        elif (aantal_items == 0) and (aantal_items_next ==0):
+            print('Looks like we hit the end of the stream, let\'s check.')
+            vorig_aantal = fetch_aantal_vergaderitems(vergaderjaar, i-1)
+
+            if (vorig_aantal != 0):
+                print('Laatste vergadering is nummer %i met %i items' % (i-1, 
+                       vorig_aantal))
+                break
+            else:
+                print('WELP, this should not be possible!')
+
+        # fetch_alle_vergaderitems(vergaderjaar, i)
+        print('Nu moeten alle vergaderitems worden gefetcht en verwerkt.')
+        # Fetch aantal vergaderitems.
+        # Als niet nul: ga verder.
+            # Als volgende niet nul: ga door, record missing data.
+        i = i + 1
+
     # FIXME: Hardcoded volgnummer for testing.
-    fetch_alle_vergaderitems(vergaderjaar, 61)
+    # fetch_alle_vergaderitems(vergaderjaar, 61)
 
 if __name__ == '__main__':
     fetch_vergaderingen_voor_jaar(20172018)
